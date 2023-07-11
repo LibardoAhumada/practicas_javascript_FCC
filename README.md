321c54c514715f037f36b2808a9716963d230de7
LibardoAhumada <134470083+LibardoAhumada@users.noreply.github.com>
2023-07-10 18:39:44 -0600

    [:ADD:]adding cards and assets media

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # practicas_javascript_FCC
-## Practicas tomadas de los videos de freecodecamp para javascript
+## Practicas tomadas de la documentación de javascript tomada de diferentes fuentes.
+### Estas practicas fueron adaptadas para que en vez de ver su resultado en la consola estos resultados pudieran ser interactivos para el usuario y pudiera usar sus propios datos para comprobar la funcionalidad del codigo.
--- javascript_prac_basics.js
+++ javascript_prac_basics.js
@@ -1,3 +1,5 @@
+
+
 let newTitle = "Titulo Agregado desde Javascript";
 document.getElementById("demo1").innerHTML = newTitle;
 
@@ -5,7 +7,8 @@ document.getElementById("demo1").innerHTML = newTitle;
 
 console.log("Hello World");
 
-document.getElementById("demo2").innerHTML = "Hello World";
+let subtitle = "Subtitulo Agregado desde JavaScript";
+document.getElementById("demo2").innerHTML = subtitle;
 
 /*++++++++++++++++++++++++++++++++++++Usuario cambiando titulo no parte de la practica+++++++++++++++++++++++++++*/
 
@@ -19,5 +22,66 @@ function changeTitle() {
   return false;
 }
 
-/* +++++++++++++++++++++++++++++++++++practica 2 crear comentario ++++++++++++++++++++++++++++++++++++++++++++*/
+/* +++++++++++++++++++++++++++++++++++practice 2 crear comentario ++++++++++++++++++++++++++++++++++++++++++++*/
+/* +++++++++++++++++++++++++++++++++++practice 3 Algoritmo Sum ++++++++++++++++++++++++++++++++++++++++++++++++*/
 
+
+function sumTwoNumbers() {
+  let num1_a = document.getElementById("num1_a").value;
+  let num2_b = document.getElementById("num1_b").value;
+  let result1 = parseFloat(num1_a) + parseFloat(num2_b);
+  document.getElementById("result1").innerHTML = result1;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+}
+
+/* +++++++++++++++++++++++++++++++++++practice 3 Algoritmo Rest ++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+function restTwoNumbers() {
+  let num2_a = document.getElementById("num2_a").value;
+  let num2_b = document.getElementById("num2_b").value;
+  let result2 = parseFloat(num2_a) - parseFloat(num2_b);
+  document.getElementById("result2").innerHTML = result2;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+}
+
+/* +++++++++++++++++++++++++++++++++++Practice 4 Multiply Algoritm  ++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+function multiTwoNumbers() {
+  let num3_a = document.getElementById("num3_a").value;
+  let num3_b = document.getElementById("num3_b").value;
+  let result3 = parseFloat(num3_a) * parseFloat(num3_b);
+  document.getElementById("result3").innerHTML = result3;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+}
+
+/* +++++++++++++++++++++++++++++++++++ Practice Divide Algoritm ++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+function divTwoNumbers() {
+  let num4_a = document.getElementById("num4_a").value;
+  let num4_b = document.getElementById("num4_b").value;
+  let result4 = parseFloat(num4_a) / parseFloat(num4_b);
+  document.getElementById("result4").innerHTML = result4;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+}
+
+/*+++++++++++++++++++++++++++++++++++Practice Potential +++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+function potTwoNumbers() {
+  let num5_a = document.getElementById("num5_a").value;
+  let num5_b = document.getElementById("num5_b").value;
+  let result5 = parseFloat(num5_a ** num5_b);
+  document.getElementById("result5").innerHTML = result5;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+
+}
+/*++++++++++++++++++++++++++++++++++Practice module +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
+function modTwoNumbers() {
+  let num6_a = document.getElementById("num6_a").value;
+  let num6_b = document.getElementById("num6_b").value;
+  let result6 = parseFloat(num6_a) % parseFloat(num6_b);
+  document.getElementById("result6").innerHTML = result6;
+  return false; // Para evitar que el formulario se envíe y la página se recargue
+}
